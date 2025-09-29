ffcb000bf69e43b40435791aa77824f624685837
Walisson Portela <78219553+walissonportela@users.noreply.github.com>
2025-09-29 14:40:08 -0300

    Axios Adjustment

--- nextbook/src/services/api.ts
+++ nextbook/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://localhost:4000/api/v1', 
-  baseURL: 'process.env.NEXT_PUBLIC_API_URL', 
+  // baseURL: 'http://localhost:4000/api/v1', // URL para desenvolvimento local
+  baseURL: process.env.NEXT_PUBLIC_API_URL,   // URL para produção (Vercel)
 });
 
 export default api;
